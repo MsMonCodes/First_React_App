e5c00151b125b73e84d4a3a5d8978e9831beda4b
MsMonCodes <ckodingthings@gmail.com>
2024-06-19 15:02:58 +0200

    My first React app

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+// // EXERCISE 01:
+
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const greeting = "Welcome to the KaasKlub!";
+//   const description = <p>This KaasKlub is going to change your life.</p>;
+
+//   return (
+//     <>
+//       <div className="card">
+//         <h1>Hello kaasling</h1>
+//         <p>{greeting}</p>
+//         {description}
+//         <p>If you don't like cheese, I wish you well.</p>
+//       </div>
+//       {/* <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p> */}
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+// EXERCISE 02:   rendering coomponents
+
+import viteLogo from '/vite.svg'
+import './App.css'
+import './components/DrinkButtons.jsx'
+import { DrinkButtons } from './components/DrinkButtons.jsx';
+
+export const App = () => {
+  const greeting = "Hello kaasling";
+  const welcome = "Welcome to the KaasKlub!";
+  const description = <p>This K.Klub is going to change your life.</p>;
+
+  return (
+    <>
+      <div className="card">
+        <h1>{greeting}</h1>
+        <h2>{welcome}</h2>
+        {description}
+        <DrinkButtons />
+        <p>If you don't like cheese, I wish you well.</p>
+      </div>
+      {/* <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
---
+++ src/components/DrinkButtons.jsx
@@ -0,0 +1,14 @@
+import './DrinkButtons.css'
+
+export const DrinkButtons = () => {
+
+    return (
+        <>
+            <h3>"Herb water or bean water?</h3>
+            <button className="button-group">
+                <button className="button">herb water</button>
+                <button className="button">bean water</button>
+            </button>
+        </>
+    );
+};
