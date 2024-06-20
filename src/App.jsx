a30eab29a062d2b7105cf7a357cee9dba3f3c934
MsMonCodes <ckodingthings@gmail.com>
2024-06-20 18:41:08 +0200

    Exercise - imprementing React state to track users choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,16 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
-// // EXERCISE 01:
-
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const greeting = "Welcome to the KaasKlub!";
-//   const description = <p>This KaasKlub is going to change your life.</p>;
-
-//   return (
-//     <>
-//       <div className="card">
-//         <h1>Hello kaasling</h1>
-//         <p>{greeting}</p>
-//         {description}
-//         <p>If you don't like cheese, I wish you well.</p>
-//       </div>
-//       {/* <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p> */}
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
-// EXERCISE 02:   rendering coomponents
-
 import viteLogo from '/vite.svg'
 import './App.css'
 import './components/DrinkButtons.jsx'
 import { DrinkButtons } from './components/DrinkButtons.jsx';
-// import { coffee, tea } from './utils/data';
 import { coffee, tea } from './utils/data.js';
 import { DrinkChoice } from './components/DrinkChoice.jsx';
-
+import { useState } from 'react';
 
 export const App = () => {
   const greeting = "Hello kaasling";
   const welcome = "Welcome to the KaasKlub!";
   const description = <p>This K.Klub is going to change your life.</p>;
-  const userDrink = coffee;
-  // const userChoice = (drink) => {
-  //   if (drink) {
-  //     reurn <DrinkChoice drink={drink} />
-  //   } else {
-  //     return 'Please select a drink';
-  //   }
-  // };
+  const [userDrink, setUerDrink] = useState(tea);
 
   return (
     <>
@@ -96,16 +18,8 @@ export const App = () => {
         <h1>{greeting}</h1>
         <h2>{welcome}</h2>
         {description}
-
-        {/* <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} /> */}
-        {/* {userDrink && <DrinkChoice drink={userDrink} />} */}
         {userDrink ? (<DrinkChoice drink={userDrink} />) : (<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />)}
-
-        {/* <p>If you don't like cheese, I wish you well.</p> */}
       </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 };
